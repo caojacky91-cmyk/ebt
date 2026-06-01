1975a5e6eca09462ebddeb0d6a37d30cd210f7b5
dinobotdev <caojacky91@gmail.com>
2026-06-01 15:52:33 +0800

    Chage function name
    prsInp -> pe

--- t.js
+++ t.js
@@ -1,2 +1,2 @@
 //Sanitizes user input to prevent XSS attacks.Convents all to plain text.@param{str} input The user input to sanitize.@returns{obj} The sanitized input.
-function prsInp(e,d=0){if(e.trim()=='')return'';if(d)console.log('Sanitizing entry: "'+e+'"');var v=(typeof e=='string')?e:e.value;return{p:new DOMParser().parseFromString(v,'text/html').documentElement.textContent,s:v.toString()};};
+function pe(e,d=0){if(e.trim()=='')return'';if(d)console.log('Sanitizing entry: "'+e+'"');var v=(typeof e=='string')?e:e.value;return{p:new DOMParser().parseFromString(v,'text/html').documentElement.textContent,s:v.toString()};};
